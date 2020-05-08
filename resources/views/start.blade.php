7095665c059d985561a1288e1b1856099ce0be3a
juniorpickle <ilyalandqvist@gmail.com>
2020-05-09 02:32:02 +0300

    Added validation for email. Added work with OpenWeatherMap.

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,210 +1,203 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Start</title>
+    <title>Start</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        <script src="https://smtpjs.com/v3/smtp.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
-        <style>
-            body {
-                text-align: center;
-                padding-top: 200px;
-            }
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-            .modal {
-                display: none;
-                position: fixed;
-                z-index: 1;
-                padding-top: 100px;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                overflow: auto;
-                background-color: rgb(0,0,0);
-                background-color: rgba(0,0,0,0.4);
-            }
+    <script src="https://smtpjs.com/v3/smtp.js"></script>
 
-            .modal-content {
-                background-color: #fefefe;
-                margin: auto;
-                padding: 20px;
-                border: 1px solid #888;
-                width: 80%;
-            }
+    <style>
+        body {
+            text-align: center;
+            padding-top: 200px;
+        }
 
-            .close {
-                color: #aaaaaa;
-                float: right;
-                font-size: 28px;
-                font-weight: bold;
-            }
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            padding-top: 100px;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgb(0,0,0);
+            background-color: rgba(0,0,0,0.4);
+        }
 
-            .close:hover,
-            .close:focus {
-                color: #000;
-                text-decoration: none;
-                cursor: pointer;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="content">
-            <div>
-                <button type="submit" id="btn" class="btn btn-success btn-lg">Старт</button>
-            </div>
+        .modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+        }
 
-            <form action="" type="POST" hidden="true" id="form" name="form">
-                {{ csrf_field() }}
-                <div class="form-group" id="formName">
-                    <label for="name">Введите имя</label>
-                    <input type='name' class="" name='name' id='name' style="width: 250px" placeholder='Введите имя' value='Name'><br>
-                </div>
-                <div class="form-group" id="formEmail">
-                    <label for="email">Введите email</label>
-                    <input type='email' class="" name='email' id='email' style="width: 250px" placeholder='Введите email' value='testtaskwork@gmail.com'><br>
-                </div>
-                <button type="button" id='btn-weather' class='btn btn-primary btn-lg'>Узнать погоду</button>
-            </form>
+        .close {
+            color: #aaaaaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
 
-            <div>
-                <p id="time"></p>
-            </div>
+        .close:hover,
+        .close:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+<div class="content">
+    <div>
+        <button type="submit" id="btn" class="btn btn-success btn-lg">Старт</button>
+    </div>
 
-            <div id="output">
-                <div hidden="true" id="hide">
-                    <div class="weather-info">
-                        <div id="location" class="info" hidden="true"></div>
-                        <div id="temperature" class="info">
-                            <span id="temperature-num"></span>
-                            <span>°</span>
-                            <span id="temperature-scale">C</span>
-                        </div>
-                        <div id="weather-condition" class="info" hidden="true"></div>
-                        <div id="weather-icon" class="info" hidden="true"></div>
-                    </div>
-                </div>
-            </div>
+    <form action="" type="POST" hidden="true" id="form" name="form">
+        {{ csrf_field() }}
+        <div class="form-group" id="formName">
+            <label for="name">Введите имя</label>
+            <input type='name' class="" name='name' id='name' style="width: 250px" placeholder='Введите имя' value='Имя' required><br>
+        </div>
+        <div class="form-group" id="formEmail">
+            <p id="noEmail"></p>
+            <label for="email">Введите email</label>
+            <input type='email' class="" name='email' id='email' style="width: 250px" placeholder='Введите email' value='testtaskwork@gmail.com' required><br>
+        </div>
+        <button type="button" id='btn-weather' class='btn btn-primary btn-lg'>Узнать погоду</button>
+    </form>
 
-            <div id="myModal" class="modal">
-                <div class="modal-content">
-                    <span class="close">&times;</span>
-                    <p>Сообщение отправлено на указанную почту</p>
-                </div>
-            </div>
-            </div>
-    </body>
+    <div>
+        <p id="time"></p>
+        <span id="temp" hidden="true"></span>
+    </div>
+
+
+
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Сообщение отправлено на указанную почту</p>
+        </div>
+    </div>
+</div>
+</body>
 </html>
 
 <script>
-    const loc = document.getElementById("location");
-    const temNum = document.getElementById("temperature-num");
-    const temScale = document.getElementById("temperature-scale");
-    const weatherCon = document.getElementById("weather-condition");
-    const weatherIcon = document.getElementById("weather-icon");
 
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position => {
-                getWeather(position.coords.latitude, position.coords.longitude);
-            });
-        } else {
-            loc.innerHTML = "Геолокация не поддерживается браузером.";
-        }
-    }
-
-    function getWeather(lat, long) {
-        const root = "https://fcc-weather-api.glitch.me/api/current?";
-        fetch(`${root}lat=${lat}&lon=${long}`, { method: "get" })
-            .then(resp => resp.json())
-            .then(data => {
-                updateDataToUI(data.name, data.weather, data.main.temp);
-            })
-            .catch(function(err) {
-                console.error(err);
-            });
-    }
-
-    var temperatureInfo = '';
-    function updateDataToUI(location, weather, temp) {
-        weatherIcon.innerHTML = `<img src="${weather[0].icon}" />`;
-        weatherCon.innerHTML = weather[0].main;
-        loc.innerHTML = location;
-        temNum.innerHTML = `${temp}`;
-        temperatureInfo = `${temp}`;
-    }
-
-    window.onload = function() {
-        getLocation();
-    };
-
+    var info = '';
     $(document).ready(function () {
+
+        let API_KEY = '7acdc0731927694027f7917e3c9fed53';
+
+        var temperature = '';
+        function getWeather(latitude, longtitude) {
+            $.ajax({
+                url: 'https://api.openweathermap.org/data/2.5/weather',
+                data: {
+                    lat: latitude,
+                    lon: longtitude,
+                    units: 'imperial',
+                    APPID: API_KEY
+                },
+                success: data => {
+                    temperature = ((5 / 9)  * (data["main"]["temp"] - 32)).toFixed(0) + " °C";
+                }
+            })
+        }
+
+        getWeather(55.751244, 37.618423);
+
+        var inputEmail = document.getElementById('email');
+        function ValidateEmail(inputEmail) {
+            var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            if(inputEmail.value.match(mailformat)) {
+                document.form.email.focus();
+                return true;
+            } else {
+                document.getElementById('noEmail').innerHTML = "Неверно введен email";
+            }
+        }
+
         $('#btn').click(function() {
             $('#btn').hide();
             document.getElementById('form').hidden = false;
             $('#btn-weather').click(function() {
-                function checkTime(i) {
-                    return (i < 10) ? "0" + i : i;
-                }
 
-                var info = '';
-                var emailReceiver = $('#email').val();
-                function startTime() {
-                    var today = new Date(),
-                        h = checkTime(today.getHours()),
-                        m = checkTime(today.getMinutes());
-                    info = "Время: " + h + ":" + m + "<br> в Москве погода ";
-                    document.getElementById('time').innerHTML = info;
-                    document.getElementById('hide').hidden = false;
-                    t = setTimeout(function () {
-                        startTime()
-                    }, 500);
-                }
-                startTime();
+                $('#email').each(function() {
+                    if(!$(this).val()){
+                        document.getElementById('noEmail').innerHTML = "Введите email, чтобы мы могли отправить Вам письмо";
+                        return false;
+                    } else if (ValidateEmail(inputEmail) == true){
 
-                Email.send({
-                    Host: "smtp.gmail.com",
-                    Username: "", // put your email here
-                    Password: "", // put your password here
-                    To: emailReceiver,
-                    From: "", // put your email here
-                    Subject: "Время и погода в Москве",
-                    Body: info + temperatureInfo + '°C',
-                }).then(
-                    $('#btn-weather').hide(),
-                    $('#formName').hide(),
-                    $('#formEmail').hide(),
-                    document.getElementById("myModal").style.display = "block"
-                );
+                        function checkTime(i) {
+                            return (i < 10) ? "0" + i : i;
+                        }
+
+                        function startTime() {
+                            var today = new Date(),
+                                h = checkTime(today.getHours()),
+                                m = checkTime(today.getMinutes());
+                            info = "Время: " + h + ":" + m + "<br> в Москве погода " + temperature;
+                            document.getElementById('time').innerHTML = info;
+                            document.getElementById('temp').hidden = false;
+                            t = setTimeout(function () {
+                                startTime()
+                            }, 500);
+                        }
+                        startTime();
+
+                        var emailReceiver = $('#email').val();
+                        function emailSend() {
+                            Email.send({
+                                SecureToken : "45c3a383-cb7a-44e0-a5bd-640e81388e01",
+                                To : emailReceiver,
+                                From : "testtaskwork@gmail.com",
+                                Subject : "Время и погода в Москве",
+                                Body : info
+                            }).then(
+                                $('#btn-weather').hide(),
+                                $('#formName').hide(),
+                                $('#formEmail').hide(),
+                                document.getElementById("myModal").style.display = "block"
+                            );
+                        }
+                        emailSend();
+                    }
+                });
             });
         });
-    });
 
-    var modal = document.getElementById("myModal");
+        var modal = document.getElementById("myModal");
 
-    var btn = document.getElementById("myBtn");
+        var btn = document.getElementById("myBtn");
 
-    var span = document.getElementsByClassName("close")[0];
+        var span = document.getElementsByClassName("close")[0];
 
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
+        span.onclick = function() {
             modal.style.display = "none";
         }
-    }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    });
 </script>
